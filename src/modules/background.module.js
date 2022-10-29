5b7e78d8c56709e3b5295656f8ad6794d1314e34
dzhangarnnn <dzhangarnnn@gmail.com>
2022-10-29 17:08:54 +0700

    add module background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,24 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
+import { random } from "../utils";
 
 export class BackgroundModule extends Module {
+    constructor(type, text) {
+        super(type, text);
+        this.symbols = '0123456789ABCDEF';
+        this.color;
+    }
 
+    #randomColor() {
+        this.color = '#'; 
+        let randomIndex;
+        for (let i = 0; i < 6; i++) {
+            randomIndex = random(0, this.symbols.length - 1);
+            this.color += this.symbols[randomIndex];
+        }
+    }
+
+    trigger() {
+        this.#randomColor();
+        document.body.style.background = this.color;
+    }
 }
