5e6f2e5d07a19066ae1fa6e9388f47a74386c9f5
igos56 <igosrus56@gmail.com>
2022-10-29 17:12:29 +0500

    working version with time selection

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,50 +1,59 @@
 import {Module} from '../core/module';
 
 export class ClicksModule extends Module {
-
-
   constructor(type, text) {
     super(type, text);
 
-    this.time = 0;
-    this.score = -1;
+    this.time;
+    this.score = 0;
 
-    this.clicksModuleWrapper = document.createElement('div');
-    this.clicksModuleWrapper.className = 'clicks-module-wrapper';
+    this.clicksModuleWrapper;
+    this.selectTimeWrapper;
+    this.timer;
+    this.timeInfoHTML;
+    this.resultInfoHTML;
+    this.againButton;
 
-    this.selectTimeWrapper = document.createElement('div');
-    this.selectTimeWrapper.className = 'select-time-wrapper';
+    document.body.addEventListener('selectstart', event => event.preventDefault());
 
-    this.timeInfoHTML = document.createElement('h1');
-    this.clicksModuleWrapper.append(this.timeInfoHTML);
+    document.body.addEventListener('click', () => {
+      if (this.time > 0) {
+        console.log('current score = ', this.score);
+        this.score += 1;
+      }
+    });
 
-    this.resultInfoHTML = document.createElement('h2');
-
-    // this.againButton = document.createElement('button');
-    // this.againButton.className = 'again-button';
-    // this.againButton.textContent = 'Еще раз';
   }
 
   finishClick() {
     this.timeInfoHTML.remove();
+
+    this.resultInfoHTML = document.createElement('h2');
+
     if (this.score === 0) {
       this.resultInfoHTML.textContent = `Странно, но ты не кликнул ни одного раза ...`;
     } else {
       this.resultInfoHTML.textContent = `Ты кликнул ${this.score} раз(а)!`;
     }
+
+    console.log('finish-click score', this.score);
     
     this.clicksModuleWrapper.append(this.resultInfoHTML);
-    // this.clicksModuleWrapper.append(this.againButton);
-    // this.againButton.addEventListener('click', () => {
-    //   this.clearModule();
-    //   this.trigger();
-    // });
 
+    this.againButton = document.createElement('button');
+    this.againButton.className = 'again-button';
+    this.againButton.textContent = 'Еще раз';
+    this.clicksModuleWrapper.append(this.againButton);
+
+    this.againButton.addEventListener('click', () => {
+      this.clearModule();
+    });
   }
 
-  // clearModule() {
-  //   this.clicksModuleWrapper.innerHTML = '';
-  // }
+  clearModule() {
+    this.clicksModuleWrapper.remove();
+    this.trigger();
+  }
 
   setTime(time) {
     this.timeInfoHTML.textContent = `Кликай! У тебя осталось 00:${time} сек.`;
@@ -52,6 +61,7 @@ export class ClicksModule extends Module {
 
   decreaseTime() {
     if (this.time === 0) {
+      clearInterval(this.timer);
       this.finishClick();
     } else {
       let currentTime = this.time--;
@@ -61,6 +71,9 @@ export class ClicksModule extends Module {
   }
 
   selectTime() {
+    this.selectTimeWrapper = document.createElement('div');
+    this.selectTimeWrapper.className = 'select-time-wrapper';
+
     this.selectTimeWrapper.innerHTML = `
       <h2>Выбери время:</h2>
       <ul class="time-list">
@@ -71,11 +84,14 @@ export class ClicksModule extends Module {
       </ul>
     `;
     this.clicksModuleWrapper.append(this.selectTimeWrapper);
+
     const timeList = this.selectTimeWrapper.querySelector('.time-list');
+
     timeList.addEventListener('click', event => {
       if (event.target.classList.contains('time-btn')) {
         const targetTime = parseInt(event.target.dataset.time);
         this.time = targetTime;
+        console.log('select-time score', this.score);
         this.selectTimeWrapper.remove();
         this.startClick();
       }
@@ -83,20 +99,36 @@ export class ClicksModule extends Module {
   }
 
   startClick() {
-    setInterval(() => {
+    console.log('start-click score', this.score);
+
+    this.timeInfoHTML = document.createElement('h1');
+    this.clicksModuleWrapper.append(this.timeInfoHTML);
+
+    this.timer = setInterval(() => {
       this.decreaseTime();
     }, 1000);
     
-    document.body.addEventListener('selectstart', event => event.preventDefault());
+    // document.body.addEventListener('selectstart', event => event.preventDefault());
 
-    document.body.addEventListener('click', () => {
-      if (this.time > 0) this.score++;
-    });
+    // document.body.addEventListener('click', () => {
+    //   if (this.time > 0) {
+    //     console.log('current score = ', this.score);
+    //     this.score += 1;
+    //   }
+    // });
   }
 
   trigger() {
-    document.body.append(this.clicksModuleWrapper);
-    this.selectTime();
+    this.score = -1;
 
+    console.log('trigger score', this.score);
+    console.log('trigger-time', this.time);
+
+    this.clicksModuleWrapper = document.createElement('div');
+    this.clicksModuleWrapper.className = 'clicks-module-wrapper';
+
+    document.body.append(this.clicksModuleWrapper);
+
+    this.selectTime();
   }
 }
