84b3e0bbe0ccb66181d4af69859777a1c6641a05
igos56 <igosrus56@gmail.com>
2022-10-29 12:31:06 +0500

    works, but with start-again problems

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -6,25 +6,23 @@ export class ClicksModule extends Module {
   constructor(type, text) {
     super(type, text);
 
-    this.time = 5;
+    this.time = 0;
     this.score = -1;
 
-    this.moduleWrapper = document.createElement('div');
-    this.moduleWrapper.style.width = '50%';
-    this.moduleWrapper.style.height = '100%';
-    this.moduleWrapper.style.marginLeft = 'auto';
-    this.moduleWrapper.style.marginRight = 'auto';
-    this.moduleWrapper.style.backgroundColor = '#dfdbdd';
-    this.moduleWrapper.style.boxShadow = '7px 7px 4px #b9b2b6';
-    this.moduleWrapper.style.borderRadius = '10px';
-    this.moduleWrapper.style.display = 'flex';
-    this.moduleWrapper.style.justifyContent = 'center';
-    this.moduleWrapper.style.alignItems = 'center';
+    this.clicksModuleWrapper = document.createElement('div');
+    this.clicksModuleWrapper.className = 'clicks-module-wrapper';
+
+    this.selectTimeWrapper = document.createElement('div');
+    this.selectTimeWrapper.className = 'select-time-wrapper';
 
     this.timeInfoHTML = document.createElement('h1');
-    this.moduleWrapper.append(this.timeInfoHTML);
+    this.clicksModuleWrapper.append(this.timeInfoHTML);
 
     this.resultInfoHTML = document.createElement('h2');
+
+    // this.againButton = document.createElement('button');
+    // this.againButton.className = 'again-button';
+    // this.againButton.textContent = 'Еще раз';
   }
 
   finishClick() {
@@ -35,8 +33,18 @@ export class ClicksModule extends Module {
       this.resultInfoHTML.textContent = `Ты кликнул ${this.score} раз(а)!`;
     }
     
-    this.moduleWrapper.append(this.resultInfoHTML);
+    this.clicksModuleWrapper.append(this.resultInfoHTML);
+    // this.clicksModuleWrapper.append(this.againButton);
+    // this.againButton.addEventListener('click', () => {
+    //   this.clearModule();
+    //   this.trigger();
+    // });
+
   }
+
+  // clearModule() {
+  //   this.clicksModuleWrapper.innerHTML = '';
+  // }
 
   setTime(time) {
     this.timeInfoHTML.textContent = `Кликай! У тебя осталось 00:${time} сек.`;
@@ -52,17 +60,43 @@ export class ClicksModule extends Module {
     }
   }
 
-  trigger() {
-    document.body.append(this.moduleWrapper);
+  selectTime() {
+    this.selectTimeWrapper.innerHTML = `
+      <h2>Выбери время:</h2>
+      <ul class="time-list">
+        <li><button class="time-btn" data-time="5">5 сек.</button></li>
+        <li><button class="time-btn" data-time="10">10 сек.</button></li>
+        <li><button class="time-btn" data-time="15">15 сек.</button></li>
+        <li><button class="time-btn" data-time="20">20 сек.</button></li>
+      </ul>
+    `;
+    this.clicksModuleWrapper.append(this.selectTimeWrapper);
+    const timeList = this.selectTimeWrapper.querySelector('.time-list');
+    timeList.addEventListener('click', event => {
+      if (event.target.classList.contains('time-btn')) {
+        const targetTime = parseInt(event.target.dataset.time);
+        this.time = targetTime;
+        this.selectTimeWrapper.remove();
+        this.startClick();
+      }
+    });
+  }
 
+  startClick() {
     setInterval(() => {
       this.decreaseTime();
     }, 1000);
-
+    
     document.body.addEventListener('selectstart', event => event.preventDefault());
 
     document.body.addEventListener('click', () => {
       if (this.time > 0) this.score++;
     });
+  }
+
+  trigger() {
+    document.body.append(this.clicksModuleWrapper);
+    this.selectTime();
+
   }
 }
