9515036170febbcdb8124bfa31624b38a349bdc6
dzhangarnnn <dzhangarnnn@gmail.com>
2022-10-29 23:54:20 +0700

    add randomColor to utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
 export function random(min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
+
+export function  getRandomColor() {
+  let color = '#'; 
+  let symbols = '0123456789ABCDEF';
+  let randomIndex;
+  for (let i = 0; i < 6; i++) {
+      randomIndex = random(0, symbols.length - 1);
+      color += symbols[randomIndex];
+  }
+  return color;
 }
