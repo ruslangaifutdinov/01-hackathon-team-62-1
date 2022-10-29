b4b85c68eb87236e6d48ce6ce3418623d509b1ce
igos56 <igosrus56@gmail.com>
2022-10-29 18:00:04 +0500

    version with click effects

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -16,27 +16,37 @@ export class ClicksModule extends Module {
 
     document.body.addEventListener('selectstart', event => event.preventDefault());
 
-    document.body.addEventListener('click', () => {
+    document.body.addEventListener('click', (event) => {
       if (this.time > 0) {
-        console.log('current score = ', this.score);
         this.score += 1;
       }
-    });
 
+      if (this.time) {
+        const cursor = document.createElement('div');
+        cursor.className = 'cursor';
+        cursor.style.top = `${event.clientY}px`;
+        cursor.style.left = `${event.clientX}px`;
+        
+        document.body.append(cursor);
+
+        setTimeout(() => {
+          cursor.remove();
+        }, 100);
+      }
+    });
   }
 
   finishClick() {
     this.timeInfoHTML.remove();
 
     this.resultInfoHTML = document.createElement('h2');
+    this.resultInfoHTML.className = 'result-title';
 
     if (this.score === 0) {
       this.resultInfoHTML.textContent = `Странно, но ты не кликнул ни одного раза ...`;
     } else {
-      this.resultInfoHTML.textContent = `Ты кликнул ${this.score} раз(а)!`;
+      this.resultInfoHTML.textContent = `Общее количество кликов - ${this.score}`;
     }
-
-    console.log('finish-click score', this.score);
     
     this.clicksModuleWrapper.append(this.resultInfoHTML);
 
@@ -73,7 +83,6 @@ export class ClicksModule extends Module {
   selectTime() {
     this.selectTimeWrapper = document.createElement('div');
     this.selectTimeWrapper.className = 'select-time-wrapper';
-
     this.selectTimeWrapper.innerHTML = `
       <h2>Выбери время:</h2>
       <ul class="time-list">
@@ -91,7 +100,6 @@ export class ClicksModule extends Module {
       if (event.target.classList.contains('time-btn')) {
         const targetTime = parseInt(event.target.dataset.time);
         this.time = targetTime;
-        console.log('select-time score', this.score);
         this.selectTimeWrapper.remove();
         this.startClick();
       }
@@ -99,30 +107,16 @@ export class ClicksModule extends Module {
   }
 
   startClick() {
-    console.log('start-click score', this.score);
-
     this.timeInfoHTML = document.createElement('h1');
     this.clicksModuleWrapper.append(this.timeInfoHTML);
 
     this.timer = setInterval(() => {
       this.decreaseTime();
     }, 1000);
-    
-    // document.body.addEventListener('selectstart', event => event.preventDefault());
-
-    // document.body.addEventListener('click', () => {
-    //   if (this.time > 0) {
-    //     console.log('current score = ', this.score);
-    //     this.score += 1;
-    //   }
-    // });
   }
 
   trigger() {
     this.score = -1;
-
-    console.log('trigger score', this.score);
-    console.log('trigger-time', this.time);
 
     this.clicksModuleWrapper = document.createElement('div');
     this.clicksModuleWrapper.className = 'clicks-module-wrapper';
