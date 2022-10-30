e0f8943b8d31e3aa716a9ae862406ac80df49c1f
GveeN <libertyhimki@gmail.com>
2022-10-30 12:28:00 +0300

    Implemented custom message module

--- src/app.js
+++ src/app.js
@@ -4,14 +4,17 @@ import { BackgroundModule } from "./modules/background.module";
 import { ShapeModule } from "./modules/shape.module";
 import { ClicksModule } from "./modules/clicks.module";
 import { SoundModule } from "./modules/sound.module";
+import { CustomMessageModule } from "@/modules/customMessage.module";
 
 const menu = new ContextMenu("#menu");
 const backgroundModule = new BackgroundModule("background", "Случайный фон");
 const shapeModule = new ShapeModule("shape", "Случайная фигура");
 const clicksModule = new ClicksModule("click", "Игра 'подсчёт кликов'");
 const soundModule = new SoundModule("sound", "Случайный звук");
+const customMessageModule = new CustomMessageModule("message", "Кастомное сообщение")
 
 menu.add(backgroundModule);
 menu.add(shapeModule);
 menu.add(clicksModule);
 menu.add(soundModule);
+menu.add(customMessageModule)
